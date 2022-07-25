f9b4795bb56b4e8dda40959bb83954d89f8e3e1a
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-07-25 13:54:59 +0300

    Mobile nav bar

---
+++ main.js
@@ -0,0 +1,33 @@
+const menuBtn = document.querySelector('.menu-btn');
+const header = document.getElementById('header');
+const navbar = document.getElementById('navbar');
+const navbarMenu = document.querySelector('.navbar-right');
+
+// burger menu action
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+// sticky navigation menu
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    // header.classList.add('sticky');
+    navbar.classList.add('sticky');
+  } else {
+    // header.classList.remove('sticky');
+    navbar.classList.remove('sticky');
+  }
+};
+
+// open menu on click
+menuBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  navbarMenu.classList.toggle('show-menu');
+});
